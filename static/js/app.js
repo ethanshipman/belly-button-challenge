e0171100e6cc1df2041d72d269a7fc4ae6ebebc7
ethanshipman <124001912+ethanshipman@users.noreply.github.com>
2025-01-06 22:37:12 -0700

    fixed key-value pairs

--- static/js/app.js
+++ static/js/app.js
@@ -17,7 +17,9 @@ function buildMetadata(sample) {
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
     Object.entries(result).forEach(([key, value]) => {
-        panel.append("h6").text('${key.toUpperCase()}: ${value}');
+        d3.select("#sample-metadata")
+        .append("h6")
+        .text('${key.toUpperCase()}: ${value}');
     });
   });
 }
